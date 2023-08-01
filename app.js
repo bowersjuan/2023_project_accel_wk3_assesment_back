97aafd2fe40ce633629d2cfdd46592097b4c029f
bowersjuan <bowersjuan@gmail.com>
2023-08-01 14:59:31 -0400

    Create get route for individual food item

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const morgan = require("morgan");
 const items = require("./data.json");
 
-app.use(cors("tiny"));
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.status(200).json({ data: "Server running..." });
@@ -12,9 +12,22 @@ app.get("/", (req, res) => {
 app.get("/items", (req, res) => {
   try {
     res.status(200).json({ data: items });
-    console.log(res);
   } catch (err) {
     res.status(500).json({ error: err });
+  }
+});
+
+app.get("/items/:id", (req, res) => {
+  const { id } = req.params;
+
+  const item = items.find((food) => {
+    return food.id === id;
+  });
+
+  if (item) {
+    res.status(200).json({ data: item });
+  } else {
+    res.status(404).json({ error: `No item with id ${id}` });
   }
 });
 
