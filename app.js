36f58a1d8ff8d155aa03502fc1649b5ea5c63756
bowersjuan <bowersjuan@gmail.com>
2023-08-04 08:31:41 -0400

    Update error handling for /items and /items/id routes

--- app.js
+++ app.js
@@ -15,21 +15,23 @@ app.get("/items", (req, res) => {
   try {
     res.status(200).json({ data: items });
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: err.message });
   }
 });
 
 app.get("/items/:id", (req, res) => {
-  const { id } = req.params;
-
-  const item = items.find((food) => {
-    return food.id === id;
-  });
-
-  if (item) {
-    res.status(200).json({ data: item });
-  } else {
-    res.status(404).json({ error: `No item with id ${id}` });
+  try {
+    const { id } = req.params;
+    const item = items.find((food) => {
+      return food.id === id;
+    });
+    if (item) {
+      res.status(200).json({ data: item });
+    } else {
+      res.status(404).json({ error: `Item with id: ${id} not found` });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
